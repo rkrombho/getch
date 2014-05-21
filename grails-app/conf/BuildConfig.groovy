f7ba8707a414284efe713f74393133b12e9b72d9
Robert Krombholz <rundeck@avgvad01x.dc-ratingen.de>
2014-05-21 16:17:42 +0200

    * added standalone plugin

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -59,6 +59,7 @@ grails.project.dependency.resolution = {
     plugins {
         // plugins for the build system only
         build ":tomcat:7.0.52.1"
+        compile ":standalone:1.2.3"
 
         // plugins for the compile step
         //compile ":scaffolding:2.0.2"
